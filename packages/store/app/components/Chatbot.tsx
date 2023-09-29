bb754cae2b2fe602caf9c8cc45829d67f0b3aa7c
delwiv <delwiv@protonmail.com>
2023-09-29 13:09:06 +0200

    feat: hide chat button if no chatbotUrl given

--- packages/store/app/components/Chatbot.tsx
+++ packages/store/app/components/Chatbot.tsx
@@ -1,23 +1,20 @@
-import { useState } from "react"
-import styles from './Chatbot.module.css'
-import { useLoaderData } from "@remix-run/react"
+import {useState} from 'react';
+import styles from './Chatbot.module.css';
+import {useLoaderData} from '@remix-run/react';
 
 export const Chatbot = () => {
-  const {chatbotUrl} = useLoaderData()
-  const [display, setDisplay] = useState(false)
+  const {chatbotUrl} = useLoaderData();
+  const [display, setDisplay] = useState(false);
 
-  const toggleDisplay = () => setDisplay(!display)
+  const toggleDisplay = () => setDisplay(!display);
 
-  console.log({chatbotUrl})
+  console.log({chatbotUrl});
   return (
-  <div className={styles.chatbot}>
-      <button onClick={toggleDisplay}>Chat</button>
-      {display && (
-      <iframe src={chatbotUrl}></iframe>
-      )}
-
-    </div>
-  )
-}
-
-
+    !!chatbotUrl && (
+      <div className={styles.chatbot}>
+        <button onClick={toggleDisplay}>Chat</button>
+        {display && <iframe title="chatbot" src={chatbotUrl}></iframe>}
+      </div>
+    )
+  );
+};
